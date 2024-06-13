0ea3bc19d8a4b5371d7e46272bd0a426296c864c
hnascx <henriquehusky@gmail.com>
2024-06-13 09:00:37 -0300

    feature/Header component concluded

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Blog } from "./pages/Blog";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Blog />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Blog />} />
+      </Route>
     </Routes>
   )
 }
